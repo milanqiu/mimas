e0b325dcfe93e87703ec0fff3216409e31e452cb
Milan Qiu <product@milanqiu.net>
2017-05-05 08:51:03 +0800

    MathUtils.getRandom and randomInRange

--- mimas-jdk-ext/src/main/java/net/milanqiu/mimas/math/MathUtils.java
+++ mimas-jdk-ext/src/main/java/net/milanqiu/mimas/math/MathUtils.java
@@ -1,5 +1,7 @@
 package net.milanqiu.mimas.math;
 
+import java.util.Random;
+
 /**
  * Utilities related to math.
  * <p>
@@ -11,4 +13,31 @@ public class MathUtils {
      * Utility class is forbidden to be instantiated.
      */
     private MathUtils() {}
+
+    private static Random random = new Random(System.currentTimeMillis());
+
+    /**
+     * Returns a singleton pseudorandom number generator. The seed has been set.
+     * @return the result pseudorandom number generator
+     */
+    public static Random getRandom() {
+        return random;
+    }
+
+    /**
+     * Returns a pseudorandom integer in the specified range.
+     * @param min the minimum of the pseudorandom integer (inclusive)
+     * @param max the maximum of the pseudorandom integer (inclusive)
+     * @return the result pseudorandom integer
+     */
+    public static int randomInRange(int min, int max) {
+        if (max == min)
+            return max;
+        if (max < min) {
+            int tmp = max;
+            max = min;
+            min = tmp;
+        }
+        return random.nextInt(max-min+1) + min;
+    }
 }
---
+++ mimas-jdk-ext/src/test/java/net/milanqiu/mimas/math/MathUtilsTest.java
@@ -0,0 +1,36 @@
+package net.milanqiu.mimas.math;
+
+import net.milanqiu.mimas.collect.ArrayUtils;
+import net.milanqiu.mimas.config.MimasJdkExtProjectConfig;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Creation Date: 2017-05-04
+ * @author Milan Qiu
+ */
+public class MathUtilsTest {
+
+    @Test
+    public void test_randomInRange() throws Exception {
+        int[] occurrence = ArrayUtils.duplicate(0, 20);
+        for (int i = 0; i < 2000; i++) {
+            if (i < 1000) {
+                int r = MathUtils.randomInRange(11, 20);
+                Assert.assertTrue(r >= 11 && r <= 20);
+                occurrence[r-11]++;
+            } else {
+                int r = MathUtils.randomInRange(30, 21);
+                Assert.assertTrue(r >= 21 && r <= 30);
+                occurrence[r-11]++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 20; i++) {
+            Assert.assertTrue(occurrence[i] > 0);
+            sb.append(i+11).append(":").append(occurrence[i]).append(System.lineSeparator());
+        }
+        MimasJdkExtProjectConfig.getSingleton().writeFileInTestOutDir(sb.toString());
+    }
+}
