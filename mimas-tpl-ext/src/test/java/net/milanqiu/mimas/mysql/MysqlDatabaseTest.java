b96d3d7c96827dae669a5ba05662ff2723fbd2ea
Milan Qiu <product@milanqiu.net>
2017-07-08 19:47:12 +0800

    Adapt for Linux

--- mimas-tpl-ext/src/test/java/net/milanqiu/mimas/mysql/MysqlDatabaseTest.java
+++ mimas-tpl-ext/src/test/java/net/milanqiu/mimas/mysql/MysqlDatabaseTest.java
@@ -2,12 +2,10 @@ package net.milanqiu.mimas.mysql;
 
 import net.milanqiu.mimas.config.MimasProperties;
 import net.milanqiu.mimas.db.DatabaseTest;
-import net.milanqiu.mimas.junit.AssertExt;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 
 /**
  * Creation Date: 2017-04-18
@@ -35,9 +33,6 @@ public class MysqlDatabaseTest extends DatabaseTest {
 
         Connection conn = null;
         try {
-            ((MysqlDatabase) db).getDbProfile().setParams("useSSL=true");
-            AssertExt.assertExceptionThrown(db::allocateConnection, SQLException.class);
-
             ((MysqlDatabase) db).getDbProfile().setParams("useSSL=true&serverTimezone=UTC&useUnicode=true&characterEncoding=gb2312");
             conn = db.allocateConnection();
             conn.createStatement().execute("CREATE TABLE t_temp (temp_id int(11) NOT NULL, temp_name varchar(50) NOT NULL, PRIMARY KEY (temp_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8");
