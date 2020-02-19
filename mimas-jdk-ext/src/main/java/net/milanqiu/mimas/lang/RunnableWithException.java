724e6f07e2e9ed4418ee36144d72ed58b10f924d
Milan Qiu <product@milanqiu.net>
2020-02-19 14:28:10 +0800

    RunnableWithException

---
+++ mimas-jdk-ext/src/main/java/net/milanqiu/mimas/lang/RunnableWithException.java
@@ -0,0 +1,17 @@
+package net.milanqiu.mimas.lang;
+
+/**
+ * Likes {@link java.lang.Runnable}, but may throw an exception when running.
+ * <p>
+ * Creation Date: 2020-02-19
+ * @author Milan Qiu
+ */
+@FunctionalInterface
+public interface RunnableWithException {
+
+    /**
+     * May take any action whatsoever, allowing exception thrown.
+     * @throws Exception any type of exception thrown out
+     */
+    void run() throws Exception;
+}
